83c8762a723280701c955d06984af65b9726807d
Wells <jesserwells@yahoo.com>
2021-12-27 11:47:19 -0600

    Fix strict and deprecated issues

--- libs/shared/data-access/api-runner/src/lib/api-runner.service.ts
+++ libs/shared/data-access/api-runner/src/lib/api-runner.service.ts
@@ -12,8 +12,8 @@ export class ApiRunner {
 			contentType?: string;
 			bearerToken?: string;
 		},
-	): Promise<T> {
-		return new Promise<T>((resolve, reject) => {
+	): Promise<T | null> {
+		return new Promise<T | null>((resolve, reject) => {
 			let headers = new HttpHeaders({
 				'Content-Type': options?.contentType ?? 'application/json',
 			});
@@ -21,27 +21,27 @@ export class ApiRunner {
 				headers = headers.set('Authorization', `Bearer ${options.bearerToken}`);
 				console.debug('set authorization', headers, options.bearerToken);
 			}
-			this.http.post(url, input, { headers: headers }).subscribe(
-				(result: any) => {
+			this.http.post(url, input, { headers: headers }).subscribe({
+				next: (result: any) => {
 					console.log('retrieved POST call', url);
 					resolve(result);
 				},
-				(error) => {
+				error: (error) => {
 					console.error('Could not execute POST call', url, input, error);
 					resolve(null);
 				},
-			);
+			});
 		});
 	}
 
-	public async callGetApi<T>(url: string): Promise<T> {
-		return new Promise<T>((resolve, reject) => {
-			this.http.get(url).subscribe(
-				(result: any) => {
+	public async callGetApi<T>(url: string): Promise<T | null> {
+		return new Promise<T | null>((resolve, reject) => {
+			this.http.get(url).subscribe({
+				next: (result: any) => {
 					console.log('retrieved GET call', url);
 					resolve(result);
 				},
-				(error) => {
+				error: (error) => {
 					// Some users have a VPN / ISP config that prevents them from accessing our static
 					// data, so there's nothing we can do unless they contact us directly
 					if (!url.includes('.json')) {
@@ -49,7 +49,7 @@ export class ApiRunner {
 					}
 					resolve(null);
 				},
-			);
+			});
 		});
 	}
 }
