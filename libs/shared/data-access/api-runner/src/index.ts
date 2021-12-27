c9cf3591b855d155e884ac8813da40d2b95f465a
Wells <jesserwells@yahoo.com>
2021-12-27 11:42:56 -0600

    Move ApiRunner to lib

--- libs/shared/data-access/api-runner/src/index.ts
+++ libs/shared/data-access/api-runner/src/index.ts
@@ -1,1 +1,2 @@
+export * from './lib/api-runner.service';
 export * from './lib/shared-data-access-api-runner.module';
--- libs/shared/data-access/api-runner/src/lib/shared-data-access-api-runner.module.ts
+++ libs/shared/data-access/api-runner/src/lib/shared-data-access-api-runner.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ApiRunner } from './api-runner.service';
 
 @NgModule({
 	imports: [CommonModule],
+	providers: [ApiRunner],
 })
 export class SharedDataAccessApiRunnerModule {}
